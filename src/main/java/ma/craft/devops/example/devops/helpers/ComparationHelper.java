9f64b6e2119b21b524ce4cbf0c58f1ff82118ee0
Sallah Kokaina <sallah.kokaina@gmail.com>
2018-09-04 14:38:24 +0100

    [sonar] - merge request issues

--- src/main/java/ma/craft/devops/example/devops/helpers/ComparationHelper.java
+++ src/main/java/ma/craft/devops/example/devops/helpers/ComparationHelper.java
@@ -2,15 +2,14 @@ package ma.craft.devops.example.devops.helpers;
 
 public class ComparationHelper {
 
+    private ComparationHelper(){}
+
     public static boolean compareLongs(long a, long b){
-        if(a == b){
-            return true;
-        }
-        return false;
+        return a == b;
     }
 
 
     public static boolean isHello(String anyString){
-        return anyString.equals("Hello");
+        return "Hello".equals(anyString);
     }
 }
